698755459f603beb43c0a0a99b9e675bfd815ce1
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-11-29 08:40:56 +0700

    Add project files.

---
+++ Design Patterns – Faceted Builder/CarInfoBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns___Faceted_Builder
+{
+    public class CarInfoBuilder : CarBuilderFacade
+    {
+        public CarInfoBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarInfoBuilder WithType(string type)
+        {
+            Car.Type = type;
+            return this;
+        }
+        public CarInfoBuilder WithColor(string color)
+        {
+            Car.Color = color;
+            return this;
+        }
+
+        public CarInfoBuilder WithNumberOfDoors(int number)
+        {
+            Car.NumberOfDoors = number;
+            return this;
+        }
+    }
+}
